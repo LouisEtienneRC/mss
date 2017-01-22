6da74124f7c663070f57eb3877b1d12bd2a4a51b
Andreas Traczyk <andreastraczyk@gmail.com>
2017-01-21 21:15:19 -0500

    initial commit

---
+++ Assets/follow.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class follow : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        var guy = GameObject.Find("guy");
+        transform.position = guy.transform.position;
+
+        var d = Input.GetAxis("Mouse ScrollWheel");
+        if (d > 0f)
+        {
+            // scroll up
+        }
+        else if (d < 0f)
+        {
+            // scroll down
+        }
+
+        transform.Translate(0,1,-20);
+    }
+}
